a99cc29a4630de702abd7626746fa6e2804aaf72
Kai Seward <kairality@gmail.com>
2022-06-08 11:56:17 -0700

    adding observation backend form

---
+++ app/forms/observation_form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, FloatField
+from wtforms.validators import DataRequired, ValidationError
+from ..models import Observation, Taxon
+
+def is_taxon(form, field):
+    # Checking if user exists
+    id = field.data
+    if not id:
+        return;
+    taxon = Taxon.query.get(id)
+    if not taxon:
+        raise ValidationError('Taxon does not exist')
+
+
+class ObservationForm(FlaskForm):
+    user_id = IntegerField('owner_id', validators=[DataRequired()])
+    taxon_id = IntegerField('taxon_id', validators=[is_taxon])
+    img_url = StringField('img_url', validators=[DataRequired()])
+    latitude = FloatField('latitude', validators=[DataRequired()])
+    longitude = FloatField('longitude', validators=[DataRequired()])
+    description = StringField('description')
