2639e36a92f56eccfb83a8fa1a7052edd6970d1f
Kai Seward <kairality@gmail.com>
2022-06-11 21:47:27 -0700

    users seeds update

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,16 +4,41 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        username='exploring_dora', email='dora@noswiping.com', password='password')
+    jobu = User(
+        username='jobu_tupaki', email='everything@everywhere.com', password='allatonce')
+    yshtola = User(
+        username='yshtola_rhul', email='yshtola@littlesun.com', password='littlesun')
+    bananamilk = User(
+        username='bananamilk', email='bananamilk@modone.com', password='banana')
+    varvarbinks = User(
+        username='var_var_binks', email='var@var.com', password='banana')
+    princesszelda = User(
+        username='princess_zelda', email='zelda@hyrule.com', password='ilovelink')
+    kipooak = User(
+        username='kipo_oak', email='kipo@wonderbeasts.com', password='dad')
+    marge = User(
+        username='marge_gunderson', email='marge@fargo.gov', password='arbys')
+    norm = User(
+        username='norm_gunderson', email='norm@normspaintings.com', password='arbys')
+    jerry = User(
+        username='jerry_lundegaard', email='jerr@gustafsonmotors.com', password='gustafson')
+    carl = User(
+        username='carl_showalter', email='carl@badguys.com', password='buscemi')
 
-    db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
-
+    db.session.add_all([
+        demo,
+        jobu,
+        yshtola,
+        bananamilk,
+        varvarbinks,
+        princesszelda,
+        kipooak,
+        marge,
+        norm,
+        jerry,
+        carl,
+    ])
     db.session.commit()
 
 
--- react-app/src/components/auth/DemoLogin.js
+++ react-app/src/components/auth/DemoLogin.js
@@ -2,20 +2,22 @@ import React from "react";
 import { useDispatch } from "react-redux";
 import { login } from "../../store/session";
 
-export default function DemoLogin({setErrors}) {
-        const dispatch = useDispatch();
-    	const demoSubmit = async (e) => {
-        e.preventDefault();
-        const data = await dispatch(login("demo@aa.io", "password"));
-        if (data) {
-          setErrors(data);
-        }
-      };
-      return <button
-                type="button"
-                className={"go-button demolition"}
-                onClick={demoSubmit}
-            >
-                Demo Login
-            </button>;
+export default function DemoLogin({ setErrors }) {
+  const dispatch = useDispatch();
+  const demoSubmit = async (e) => {
+    e.preventDefault();
+    const data = await dispatch(login("dora@noswiping.com", "password"));
+    if (data) {
+      setErrors(data);
+    }
+  };
+  return (
+    <button
+      type="button"
+      className={"go-button demolition"}
+      onClick={demoSubmit}
+    >
+      Demo Login
+    </button>
+  );
 }
