d6ba9e7ad9e18853fe800da586c957e7172067e1
Kai Seward <kairality@gmail.com>
2022-06-12 20:58:10 -0700

    shooting in the dark

--- app/api/identification_routes.py
+++ app/api/identification_routes.py
@@ -24,7 +24,7 @@ def recalculate_observation(observation_id):
         observation.verified = False
     db.session.add(observation)
     db.session.commit()
-    return Observation.query.get(id)
+    return observation;
 
 
 @identification_routes.route('/')
