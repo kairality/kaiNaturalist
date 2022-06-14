3947d5b8d8d2e93642906e1810ff846f926f2421
Kai Seward <kairality@gmail.com>
2022-06-14 08:49:52 -0700

    added additional user routes

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
-from app.models import User
+from app.models import User, Observation, Identification
 
 user_routes = Blueprint('users', __name__)
 
@@ -17,3 +17,15 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+@user_routes.route('/<int:id>/observations')
+@login_required
+def get_user_observations(id):
+    observations = Observation.query.filter(Observation.user_id == id).all()
+    return {"observations": [observation.id for observation in observations]}
+
+@user_routes.route('/<int:id>/identifications')
+@login_required
+def get_user_identifications(id):
+    identifications = Identification.query.filter(Identification.user_id == id).all()
+    return {"identifications": [identification.id for identification in identifications]}
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -2,9 +2,12 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { login } from "../../store/session";
+import ErrorCard from "../ErrorCard/ErrorCard";
 import DemoLogin from "./DemoLogin";
 
-const LoginForm = () => {
+import "./LoginForm.css";
+
+export default function LoginForm() {
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -32,36 +35,54 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={onLogin}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
+    <div className="splash-wrapper">
+      <div className="splash">
+        Test content
       </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="text"
-          placeholder="Email"
-          value={email}
-          onChange={updateEmail}
-        />
+      <div className="splash-border">
+        kaiNaturalist is not a joint initiative with anyone.
       </div>
-      <div>
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={updatePassword}
-        />
-        <button type="submit">Login</button>
-        <DemoLogin {...{ setErrors }} />
-      </div>
-    </form>
+      <form className="auth-form" onSubmit={onLogin}>
+        <div className="login-text">
+          <h3 id="h3-login">Connect with nature</h3>
+          <h4 id="h4-login">But first, you must connect to your account.</h4>
+        </div>
+        <div className="form-errors center">
+          <ErrorCard {...{ errors }} />
+        </div>
+        <div className="auth-fields">
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={updateEmail}
+          />
+        </div>
+        <div className="auth-fields">
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={updatePassword}
+          />
+          <div>
+            <div className="login-buttons">
+              <button className="go-button" type="submit">
+                Login
+              </button>
+              <DemoLogin />
+            </div>
+            <p>Don't have an account?</p>
+            <button id="to-sign-up" className="go-button" onClick={() => {}}>
+              Sign up!
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
-
-export default LoginForm;
