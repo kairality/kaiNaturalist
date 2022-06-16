9b462029eba2f5d7c72bec201f3c7ab6fc56f1dc
Kai Seward <kairality@gmail.com>
2022-06-16 11:44:32 -0700

    partition homepage into mine, others, needs ID

--- react-app/src/components/NaturalistHome/NaturalistHome.js
+++ react-app/src/components/NaturalistHome/NaturalistHome.js
@@ -9,11 +9,11 @@ import { genObservations } from "../../store/observation";
 import Loader from "../Loader/Loader";
 
 export default function NaturalistHome() {
-
-  const observations = useSelector((state) => state.observations)
-  const taxa = useSelector((state) => state.taxonomy)
+  const user = useSelector((state) => state.session.user);
+  const observations = useSelector((state) => state.observations);
+  const taxa = useSelector((state) => state.taxonomy);
   const [loaded, setLoaded] = useState(false);
-  const [taxaLoaded, setTaxaLoaded] = useState(taxa?.[0])
+  const [taxaLoaded, setTaxaLoaded] = useState(taxa?.[0]);
 
   const dispatch = useDispatch();
 
@@ -21,7 +21,7 @@ export default function NaturalistHome() {
     (async () => {
       const _observations = await dispatch(genObservations());
       if (_observations) {
-        setLoaded(true)
+        setLoaded(true);
       }
     })();
   }, [dispatch]);
@@ -30,24 +30,79 @@ export default function NaturalistHome() {
     if (Object.keys(taxa).length) {
       setTaxaLoaded(true);
     }
-  },[taxa])
+  }, [taxa]);
 
-  if (!loaded || !taxaLoaded) {
+  if (!loaded || !taxaLoaded || !user) {
     return (
-    <div className="home-loading" id="home-container">
-      <Loader loadingText={"Initializing Taxonomy"}/>
-    </div>
+      <div className="home-loading" id="home-container">
+        <Loader loadingText={"Initializing Taxonomy"} />
+      </div>
     );
   }
 
+
+  // partition observations into "My observations", "observations that need ID", and "everything else"
+  const [myObservations, othersObservations, needsIDObservations] =
+    Object.values(observations).reduce(
+      ([mine, others, needsID], observation) => {
+        if (observation.user_id === user.id) {
+          return [[...mine, observation], others, needsID];
+        } else if (!observation.verified) {
+          return [mine, others, [...needsID, observation]];
+        } else {
+          return [mine, [...others, observation], needsID];
+        }
+      },
+      [[], [], []]
+    );
+
   return (
     <div id="home-container">
-      <h1>Recent Observations</h1>
-      <div class="observation-display">
-        {Object.values(observations).map((observation) => {
-          return <ObservationCard key={`observation-${observation.id}`} observation={observation} />;
-        })}
-      </div>
+      {myObservations.length > 0 && (
+        <>
+          <h1>My Observations</h1>
+          <div class="observation-display">
+            {myObservations.map((observation) => {
+              return (
+                <ObservationCard
+                  key={`observation-${observation.id}`}
+                  observation={observation}
+                />
+              );
+            })}
+          </div>
+        </>
+      )}
+      {needsIDObservations.length > 0 && (
+        <>
+          <h1>Observations Needing Verification</h1>
+          <div class="observation-display">
+            {needsIDObservations.map((observation) => {
+              return (
+                <ObservationCard
+                  key={`observation-${observation.id}`}
+                  observation={observation}
+                />
+              );
+            })}
+          </div>
+        </>
+      )}
+      {othersObservations.length > 0 && (
+        <>
+          <h1>Recent Community IDs</h1>
+          <div class="observation-display">
+            {othersObservations.map((observation) => {
+              return (
+                <ObservationCard
+                  key={`observation-${observation.id}`}
+                  observation={observation}
+                />
+              );
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
-};
+}
