57dc292a29073b16a294504c2d336a3679174863
Kai Seward <kairality@gmail.com>
2022-06-18 17:56:09 -0700

    fix repo link

--- react-app/src/components/AboutLinks/GitHubRepoLink.js
+++ react-app/src/components/AboutLinks/GitHubRepoLink.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faGithubAlt} from "@fortawesome/free-brands-svg-icons";
-import {faCodeBranch} from "@fortawesome/free-solid-svg-icons";
-export const gitHubRepoUrl = "https://github.com/kairality";
+import { faCodeBranch } from "@fortawesome/free-solid-svg-icons";
+export const gitHubRepoUrl = "https://github.com/kairality/kaiNaturalist";
 
 export default function GitHubRepoLink() {
-    return (
+  return (
     <div className="about-link github-repo-link">
-        <a href={gitHubRepoUrl} target="_blank" rel="noreferrer">
-           <FontAwesomeIcon icon={faCodeBranch} />
-           {"kaiNaturalist @ github"}
-        </a>
+      <a href={gitHubRepoUrl} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faCodeBranch} />
+        {"kaiNaturalist @ github"}
+      </a>
     </div>
-    );
+  );
 }
--- react-app/src/components/AboutLinks/LinkedInLink.js
+++ react-app/src/components/AboutLinks/LinkedInLink.js
@@ -2,15 +2,15 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-export const linkedInUrl = "https://www.linkedin.com/in/kai-seward-01338b95/"
+export const linkedInUrl = "https://www.linkedin.com/in/kai-seward-01338b95/";
 
 export default function LinkedinLink() {
-    return(
-  <div className="about-link linkedin-link">
-    <a href={linkedInUrl} target="_blank" rel="noreferrer">
-      <FontAwesomeIcon icon={faLinkedin} />
-      {"Kai Seward @ LinkedIn"}
-    </a>
-  </div>
-    );
+  return (
+    <div className="about-link linkedin-link">
+      <a href={linkedInUrl} target="_blank" rel="noreferrer">
+        <FontAwesomeIcon icon={faLinkedin} />
+        {"Kai Seward @ LinkedIn"}
+      </a>
+    </div>
+  );
 }
